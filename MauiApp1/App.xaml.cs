4cb0d08c1336dafb517d4918bb83aef7548469b8
Damian Wolgast <damian.wolgast@scits.net>
2025-01-21 19:05:40 +0100

    1st

---
+++ MauiApp1/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace MauiApp1;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        var window = new Window(new AppShell());
+
+        window.SizeChanged += (sender, args) =>
+        {
+            if (sender is AppShell shell)
+            {
+                Console.WriteLine($"X Y W H: {shell.X} {shell.Y} {shell.Width} {shell.Height}");
+            }
+        };
+
+        return window;
+    }
+}
---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using CoreGraphics;
+using UIKit;
+
+namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
+
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times";
+
+        Console.WriteLine(
+            $"UIApplication.SharedApplication.ConnectedScenes.OfType<UIWindowScene>().FirstOrDefault().Windows[0].Frame.X:" +
+            $"{UIApplication.SharedApplication.ConnectedScenes.OfType<UIWindowScene>().FirstOrDefault().Windows[0].Frame.X}");
+
+        Console.WriteLine(
+            $"UIApplication.SharedApplication.ConnectedScenes.OfType<UIWindowScene>().FirstOrDefault().Windows[0].Screen.FixedCoordinateSpace.Bounds.Width:" +
+            $"{UIApplication.SharedApplication.ConnectedScenes.OfType<UIWindowScene>().FirstOrDefault().Windows[0].Screen.FixedCoordinateSpace.Bounds.X}");
+
+        Console.WriteLine(
+            $"UIApplication.SharedApplication.ConnectedScenes.OfType<UIWindowScene>().FirstOrDefault().Windows[0].Screen.Bounds.X:" +
+            $"{UIApplication.SharedApplication.ConnectedScenes.OfType<UIWindowScene>().FirstOrDefault().Windows[0].Screen.Bounds.X}");
+
+        Console.WriteLine(
+            $"UIApplication.SharedApplication.Windows[0].Frame.Height: {UIApplication.SharedApplication.Windows[0].Frame.Height}");
+
+        Console.WriteLine(
+            $"UIApplication.SharedApplication.Windows[0].Frame.X: {UIApplication.SharedApplication.Windows[0].Frame.X}");
+
+        Console.WriteLine(
+            $"UIApplication.SharedApplication.Windows[0].Frame.GetMaxX(): {UIApplication.SharedApplication.Windows[0].Frame.GetMaxX()}");
+
+        Console.WriteLine(
+            $"UIApplication.SharedApplication.Windows[0].WindowLevel: {UIApplication.SharedApplication.Windows[0].WindowLevel}");
+
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+}
